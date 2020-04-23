483c6e07d687e548718eac1b5b67c522a7e82b97
Paul Sorensen <paulsorensen5@gmail.com>
2020-04-22 18:36:36 -0700

    Add Not 8 and 16 bit bus

---
+++ Not16bit.v
@@ -0,0 +1,15 @@
+// |    in    |   out    |
+// | 00000000 | 11111111 |
+// | 11111111 | 00000000 |
+// | 10101010 | 01010101 |
+// | 00111100 | 11000011 |
+// | 00010010 | 11101101 |
+
+module Not16bit(out, in);
+    output wire out[15:0];
+    input  wire in[15:0];
+
+    Not8bit bit0_7(.out(out[7:0]), .in(in[7:0]));
+    Not8bit bit8_15(.out(out[15:8]), .in(in[15:8]));
+endmodule
+}
---
+++ Not8bit.v
@@ -0,0 +1,21 @@
+// |    in    |   out    |
+// | 00000000 | 11111111 |
+// | 11111111 | 00000000 |
+// | 10101010 | 01010101 |
+// | 00111100 | 11000011 |
+// | 00010010 | 11101101 |
+
+module Not8bit(out, in);
+    output wire out[7:0];
+    input  wire in[7:0];
+
+    Not bit0(.out(out[0]), .in(in[0]));
+    Not bit1(.out(out[1]), .in(in[1]));
+    Not bit2(.out(out[2]), .in(in[2]));
+    Not bit3(.out(out[3]), .in(in[3]));
+    Not bit4(.out(out[4]), .in(in[4]));
+    Not bit5(.out(out[5]), .in(in[5]));
+    Not bit6(.out(out[6]), .in(in[6]));
+    Not bit7(.out(out[7]), .in(in[7]));
+endmodule
+}
